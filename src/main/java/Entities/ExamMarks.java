2e3c272b78a8d4bad8e34c8311b97c82eba58788
muddassirlateef <muddassirlateef001@gmail.com>
2022-05-15 17:31:15 +0500

    Update Marks Implemented

--- src/main/java/Entities/ExamMarks.java
+++ src/main/java/Entities/ExamMarks.java
@@ -9,5 +9,34 @@ public class ExamMarks {
     @GeneratedValue(strategy=GenerationType.AUTO)
     @Column(name = "marksID", nullable = false)
     private int marksID;
+    @Column(name = "studentName", nullable = false)
+    private String studentName;
+    @Column(name = "percentage", nullable = false)
+    private String percentage;
+    @Column(name = "subject", nullable = false)
+    private String subject;
 
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(String percentage) {
+        this.percentage = percentage;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
